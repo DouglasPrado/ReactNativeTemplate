cf661bb7fbb3a3e04b1739cce438319b92c401dd
Douglas Prado <douglasrafaeldoprado@gmail.com>
2019-04-03 17:17:38 -0300

    Splash test lottie

--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -1,10 +1,44 @@
 import React, { Component } from "react";
 import { Container, Text } from "./style";
+import LottieView from "lottie-react-native";
+import Svg, { Circle, Rect } from "react-native-svg";
+import SvgUri from "react-native-svg-uri";
 export default class SplashScreen extends Component {
   render() {
     return (
       <Container>
-        <Text>Template Exemplo</Text>
+        <LottieView
+          style={{ marginBottom: 40 }}
+          source={require("../../assets/favorite_black.json")}
+          autoPlay
+        />
+        <SvgUri
+          width="200"
+          height="200"
+          source={{
+            uri:
+              "http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg"
+          }}
+        />
+        <Svg height="50%" width="50%" viewBox="0 0 100 100">
+          <Circle
+            cx="50"
+            cy="50"
+            r="45"
+            stroke="blue"
+            strokeWidth="2.5"
+            fill="green"
+          />
+          <Rect
+            x="15"
+            y="15"
+            width="70"
+            height="70"
+            stroke="red"
+            strokeWidth="2"
+            fill="yellow"
+          />
+        </Svg>
       </Container>
     );
   }
--- src/screens/Splash/style.js
+++ src/screens/Splash/style.js
@@ -2,13 +2,8 @@ import React from "react";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  display: flex;
-  flex: 1;
-  background: #dbdbdb;
-  justify-content: center;
+  margin-top: 70px;
+  background: #fff;
+  justify-content: space-between;
   align-items: center;
-`;
-export const Text = styled.Text`
-  color: #fbfbfb;
-  font-size: 16px;
 `;
